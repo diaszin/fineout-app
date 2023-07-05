8acb9514a77dac2b204891ad27e4c81748885928
diaszin <84644190+diaszin@users.noreply.github.com>
2023-07-05 19:23:19 -0300

    Criei a classe, porém não terminei

---
+++ lib/model/User.dart
@@ -0,0 +1,9 @@
+import 'package:fineout_app/db.dart';
+
+class User {
+  final _banco = meuBanco();
+  Future<bool> createUser() async {
+    var instanceBanco = await _banco;
+    return false;
+  }
+}
